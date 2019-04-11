fe6632b4f179a077c19b0ca3247e4dbbe4e772c1
Cássio Gmarra <cassio1515@hotmail.com>
2019-04-10 21:33:29 -0300

    Update exe6.c

--- exe6.c
+++ exe6.c
@@ -15,22 +15,37 @@ typedef struct no{
 
 Celula *inserir(int valor, Celula *l);
 Celula *excluir(Celula *l);
+Celula *destruir(Celula *l) ;
+int contar(Celula *l);
 void exibir(Celula *l);
 
 int main(){
 	Celula *lista = NULL;
-
+    int tam;
 	srand(time(NULL));
 
-	for(int i = 0; i < 20; i++){
+    do{
+        system("cls");
+        printf("\nQual o tamanho da lista?");
+        scanf("%d", &tam);
+    }while(tam <= 0);
+
+	for(int i = 0; i < tam; i++){
 		lista=(inserir(rand()%100, lista));
 	}
-	printf("Lista original:\n");
+
+	printf("\nLista original:\n");
 	exibir(lista);
     lista = excluir(lista);
-    printf("\n\nNova lista:\n");
 
-   	exibir(lista);
+    if(contar(lista) == 0){
+        printf("\n\nA lista ficou vazia!");
+    }
+    else{
+        printf("\n\nNova lista:\n");
+        exibir(lista);
+    }
+
 	return 1;
 }
 
@@ -67,11 +82,15 @@ Celula *excluir(Celula *l){
 	Celula *p, *pR;
 	if(!l) return l;
 
-    p = l;
-    l = l->prox;
-    free(p);
+    if(contar(l) < 3){
+        l = destruir(l);
+    }
 
-    if(l->prox){
+    else{
+        p = l;
+        l = l->prox;
+        free(p);
+
         for(pR = NULL, p = l; p; pR = p, p = p->prox){
             if(!p->prox){
                 free(p);
@@ -80,7 +99,31 @@ Celula *excluir(Celula *l){
             }
         }
     }
+
     return l;
+}
+
+//Destruir a lista
+Celula *destruir(Celula *l){
+	Celula *p, *pR;
+	if (!l) return l;
+
+	for (pR = l, p = l->prox; p; pR = p, p = p->prox) {
+		free(pR);
+	}
+	free(pR);
+	return NULL;
+}
+
+//Conta os números da lista
+int contar(Celula *l){
+	Celula *p;
+	int cont = 0;
+
+	for(p = l; p; p = p->prox, cont++){
+
+	}
+	return cont;
 }
 
 //Exibir os elementos da lista
