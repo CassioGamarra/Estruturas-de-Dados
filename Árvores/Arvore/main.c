349cae20237a1a0232f96da02abced1d4e2fc9af
Cássio Gamarra <cassio1515@hotmail.com>
2019-06-11 11:58:21 -0300

    Arvores

--- Árvores/Arvore/main.c
+++ Árvores/Arvore/main.c
@@ -11,20 +11,34 @@ Arvore *inserir(int valor, Arvore *r);
 void exibirRED(Arvore *r);
 void exibirERD(Arvore *r);
 void exibirEDR(Arvore *r);
+void exibirArvore(Arvore *r, int l);
+void mostrarFolhas(Arvore *r);
+Arvore *pesquisar(Arvore *r, int valor);
+
+int contarNos(Arvore* r);
+
 int main(){
     Arvore *raiz = NULL;
+    int valor;
+    srand(time(NULL));
+    for(int i = 0; i < 10; i++){
+        raiz = inserir(rand()%100, raiz);
+    }
 
-    raiz = inserir(15, raiz);
-    raiz = inserir(5, raiz);
-    raiz = inserir(2, raiz);
-    raiz = inserir(10, raiz);
-    raiz = inserir(20, raiz);
     printf("Exibir RED:\n");
     exibirRED(raiz);
     printf("\nExibir ERD:\n");
     exibirERD(raiz);
     printf("\nExibir EDR:\n");
     exibirEDR(raiz);
+    printf("\nExibir Arvore:\n");
+    exibirArvore(raiz, 0);
+    printf("\nNumero de nos: %d\n", contarNos(raiz));
+    printf("\nExibir folhas:\n");
+    mostrarFolhas(raiz);
+    printf("\n\nDigite um valor para pesquisar: ");
+    scanf("%d", &valor);
+    pesquisar(raiz, valor);
     return 0;
 }
 
@@ -73,4 +87,56 @@ void exibirEDR(Arvore *r){
         exibirEDR(r->dir);
         printf("%d \t", r->dado);
     }
+}
+
+//Exibir com EDR
+void exibirArvore(Arvore *r, int nivel){
+    int i;
+    if(r){
+        exibirArvore(r->dir, nivel+1);
+        for(i = 0; i < nivel; i++){
+            printf("   ");
+        }
+        printf("%d\n", r->dado);
+        exibirArvore(r->esq, nivel+1);
+    }
+}
+
+//Contar os nós
+int contarNos(Arvore *r){
+    int cont;
+    if(r){
+        return 1 + contarNos(r->dir)+contarNos(r->esq);
+    }
+}
+
+//Mostrar Folhas
+void mostrarFolhas(Arvore *r){
+    if(r){
+        if(!r->dir && !r->esq){
+            printf("%d ", r->dado);
+        }
+        if(r->esq){
+            mostrarFolhas(r->esq);
+        }
+        if(r->dir){
+            mostrarFolhas(r->dir);
+        }
+    }
+}
+
+//Pesquisar
+Arvore *pesquisar(Arvore *r, int valor){
+    if(r){
+        if(r->dado == valor){
+            return r;
+        }
+        if(r->esq){
+            pesquisar(r->esq, valor);
+        }
+        if(r->dir){
+            pesquisar(r->dir, valor);
+        }
+    }
+    return NULL;
 }
